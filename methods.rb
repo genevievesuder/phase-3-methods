529368ea5c793cbe1e313742179047c429bf906b
Genevieve Suder <genevievesuder@gmail.com>
2023-01-09 08:33:59 -0800

    commit tree e7a7a09fd628e99cb4924464996f2d8c95c960fb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+# Module - Methods
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+
+def halve num1
+    if num1.is_a?(Integer)
+      num1 / 2
+    else
+      nil
+    end
+end
